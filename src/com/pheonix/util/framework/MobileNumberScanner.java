a5ec7b14c48df60021ae3b5fc044851fd61173c8
Anurag Kumar <AKumar@actiance.com>
2018-02-09 19:54:26 +0530

    Test

--- src/com/pheonix/util/framework/MobileNumberScanner.java
+++ src/com/pheonix/util/framework/MobileNumberScanner.java
@@ -1,6 +1,6 @@
 package com.pheonix.util.framework;
 
-public class IndiaPhoneScanner {
+public class MobileNumberScanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
